bb413da0d7241cb98c598f75b22357c11f7841a6
abdenour chenouf <abdenour.chenouf23@ump.ac.ma>
2024-12-17 12:24:58 +0100

    first commit

--- dags/extract1.py
+++ dags/extract1.py
@@ -1,2 +1,18 @@
-import request
-import pandas as pd
+import requests
+
+url = "https://linkedin-data-scraper.p.rapidapi.com/search_jobs"
+
+payload = {
+	"keywords": "Data scientist",
+	"location": "morocco",
+	"count": 50
+}
+headers = {
+	"x-rapidapi-key": "7bbaa81c63msh190363b3189eb05p10f6d2jsne405b9c1ded2",
+	"x-rapidapi-host": "linkedin-data-scraper.p.rapidapi.com",
+	"Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())

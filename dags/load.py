55bc6a0a2a2428170a2531a3ca73d04886310105
abdenour234 <abdenour.chenouf23@ump.ac.ma>
2025-02-07 22:00:53 +0100

    hola como estas

--- dags/load.py
+++ dags/load.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 from utilities import upload_dataframe_to_blob
 
@@ -26,7 +27,7 @@ def load(**kwargs):
         df,
         connection_string="DefaultEndpointsProtocol=https;AccountName=myaccount123xyz;AccountKey=xhK3fQlCsEhvhkYeB4T4R2i6+kKB9vNPNKCkELyp87bo4BYnf9ZiZAWWXf0XeyhgKY3c1CZtK6jL+AStQynDEg==;EndpointSuffix=core.windows.net",
         container_name="gold",
-        blob_name="data/data_gold.csv"
+        blob_name=f"data/data_gold_{datetime.datetime.now()}.csv"
     )
     
     return df
